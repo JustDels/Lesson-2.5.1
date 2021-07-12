3ef1f56d7414a7e6a79a16a22ff3ae5f72b3e0ce
Dmitry Toporkov <dels.axe@gmail.com>
2021-07-12 16:44:52 +0500

    100% coverage

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,18 +1,34 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
-  /**
-   * Calculate index of max income
-   *
-   * @param incomes - array of incomes
-   * @return - index of first max value
-   */
-  public long findMax(long[] incomes) {
-    long current_max_index = 0;
-    long current_max = incomes[0];
-    for (long income : incomes)
-      if (current_max < income)
-        current_max = income;
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
+    public long findMax(long[] incomes) {
+        long current_max = incomes[0];
+        for (long income : incomes) {
+            System.out.println(income);
+        }
         return current_max;
-  }
+    }
+
+    public long findMin(long[] incomes) {
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+    }
+
+    public long calculateMid(long[] incomes) {
+        long midl = incomes[0];
+        long month = 11;
+        for (long income : incomes) {
+            midl += income;
+        }
+        return midl / month;
+    }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,15 +6,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+        long actual = service.findMin(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 
-    long actual = service.findMax(incomesInBillions);
-
-    assertEquals(expected, actual);
-  }
+    @Test
+    void calculateMid() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 8;
+        long actual = service.calculateMid(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 }
